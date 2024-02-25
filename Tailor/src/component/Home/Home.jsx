b955eb3fbeeaa64325de5be2be5b19a3ce8b6c12
Nil <95134227+NirejBatrai@users.noreply.github.com>
2024-02-25 20:34:05 +0700

    added animation

--- Tailor/src/component/Home/Home.jsx
+++ Tailor/src/component/Home/Home.jsx
@@ -5,15 +5,15 @@ import "./Home.css";
 import Contact from "../Contact/Contact";
 import About from "../About/About";
 import Footer from "../footer/Footer";
-
+import shirt from "../../assets/shirt.png";
 function Home() {
   return (
     <>
       <div className='container'>
-        <section className='home-container'>Home Page</section>
+        <section className='home-container'></section>
+        <h1>Welcome to our tailor</h1>
+        <img src={shirt} alt='My Image' className='img' />
       </div>
-      <About />
-      <Contact />
     </>
   );
 }
